fd0f02e1c95e309ef8700059bad55ce09d7929b9
Bhavya27092027 <bhavya.23b0311201@abes.ac.in>
2025-11-03 09:43:08 +0530

    React props

--- ReactWork/ReactViteApp/reactiteapp/ReactViteProps/React-Props/src/App.jsx
+++ ReactWork/ReactViteApp/reactiteapp/ReactViteProps/React-Props/src/App.jsx
@@ -2,10 +2,34 @@ import './App.css'
 import Student from './components/Student'
 
 function App() {
+  const student = [{
+    pic: 'https://static.vecteezy.com/system/resources/previews/001/967/165/original/child-go-to-the-school-free-vector.jpg',
+    name: 'Bhavya Jain',
+    branch: 'AIML',
+    section: 'B',
+    roll_no: '2300320310068',
+    college: 'ABES Engineering College'
+  },
+  {
+    pic: 'https://en.pimg.jp/106/089/766/1/106089766.jpg',
+    name: 'Priyank Jain',
+    branch: 'MBA FINANCE',
+    section: 'A',
+    roll_no: '2300320310052',
+    college: 'ITS Engineering College'
+  }
+  ]
 
   return (
     <div>
-      <Student name="Bhavya Jain" branch="AIML" section="B" roll_no="2300320310068" college="ABES Engineering College" />
+      {/* <Student name="Bhavya Jain" branch="AIML" section="B" roll_no="2300320310068" college="ABES Engineering College" /> */}
+      {/* <Student data={student[1]} /> */}
+
+      {
+        student.map((element, index) => (
+          <Student data={element} key={index} />
+        ))
+      }
     </div>
   )
 }
--- ReactWork/ReactViteApp/reactiteapp/ReactViteProps/React-Props/src/components/Student.jsx
+++ ReactWork/ReactViteApp/reactiteapp/ReactViteProps/React-Props/src/components/Student.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './student.css'
 
-const Student = (props) => {
+const Student = ({ data }) => {
     return (
         <div className='page-wrapper'>
             <h1>Student Info</h1>
-            <h3>Name: {props.name}</h3>
-            <h3>Branch: {props.branch}</h3>
-            <h3>Section: {props.section}</h3>
-            <h3>Roll_Number: {props.roll_no}</h3>
-            <h3>College: {props.college}</h3>
+            <img style={{ borderRadius: "50%" }} src="data.pic" height={200} width={200} alt="Profile Image" />
+            <h3>Name: {data.name}</h3>
+            <h3>Branch: {data.branch}</h3>
+            <h3>Section: {data.section}</h3>
+            <h3>Roll_Number: {data.roll_no}</h3>
+            <h3>College: {data.college}</h3>
         </div>
     );
 }
